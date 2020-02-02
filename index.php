85587bb883dc8bf7a317147e6fdb49d438a5b1ff
iridium822 <tanuha253@tut.by>
2020-02-02 15:07:47 +0300

    Add packages to composer

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+// Autoload classes
+spl_autoload_register(function ($class) {
+    require_once 'core/' . $class . '.php';
+});
+
+// Load package
+require_once 'vendor/autoload.php';
+
+//Load project config
+require_once 'cfg/config.php';
+
+// Run project
+Route::start();
